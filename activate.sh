5c0fa4cf29c46d0ebb73b2d1baf9e67c3e847a69
Dan Adiletta <dan.adiletta@gmail.com>
2018-05-02 13:10:01 +0000

    Added more verbose info to activate.sh

--- activate.sh
+++ activate.sh
@@ -5,7 +5,8 @@ cd ~/workspace/
 
 # Activate the virtual environment(venv)
 source venv/bin/activate
-
+echo "* Activate VirtualEnvironment (venv)"
+echo " ...done"
 #Activate postgresql
 
 sudo service postgresql start
